9d0d404db93812c274181f8457f3231a526f7771
buzznutbrainz <ava.ehtesham-heravi@edu.sait.ca>
2024-12-11 14:36:35 -0700

    media queries

--- buzznut.js
+++ buzznut.js
@@ -7,60 +7,12 @@ navToggle.addEventListener('click', () => {
 });
 
 
-
-
-
-//portfolio carousel//
-const portfolioImages = document.querySelectorAll(".portfolio img");
-const carouselLightbox = document.getElementById("carousel-lightbox");
-const carouselImage = document.getElementById("carousel-image");
-const prevButton = document.getElementById("prev");
-const nextButton = document.getElementById("next");
-const closeCarousel = document.getElementById("close-carousel");
-
-let currentIndex = 0;
-
-portfolioImages.forEach((img, index) => {
-    img.addEventListener("click", () => {
-        currentIndex = index;
-        carouselImage.src = img.src;
-        carouselImage.alt = img.alt; 
-        carouselLightbox.style.display = "flex"; 
-    });
-});
-
-// close carousel
-closeCarousel.addEventListener("click", () => {
-    carouselLightbox.style.display = "none";
-});
-
-//  next image
-nextButton.addEventListener("click", () => {
-    currentIndex = (currentIndex + 1) % portfolioImages.length;
-    carouselImage.src = portfolioImages[currentIndex].src;
-});
-
-//  previous image
-prevButton.addEventListener("click", () => {
-    currentIndex = (currentIndex - 1 + portfolioImages.length) % portfolioImages.length;
-    carouselImage.src = portfolioImages[currentIndex].src;
-});
-
-
 //back to top button"
 
 const backToTopButton = document.getElementById('back-to-top');
 
-// show/hide the button based on scroll position
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 300) {
-        backToTopButton.style.display = 'block';
-    } else {
-        backToTopButton.style.display = 'none';
-    }
-});
 
-// Scroll smoothly to the top when the button is clicked
+// Scroll smoothly
 backToTopButton.addEventListener('click', () => {
     window.scrollTo({
         top: 0,
